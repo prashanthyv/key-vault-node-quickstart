61082e0283894bf2c63fc98e14261da45ca22cbd
Prashanth <yvprashanth@gmail.com>
2018-07-24 11:51:25 -0700

    More fixes

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
+
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Prashanth!");
+    response.end("Hello World!");
+
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World!!!');
-});
-  
 var port = process.env.PORT || 1337;
-// server.listen(port);
-// console.log("Server running at http://localhost:%d", port);
+server.listen(port);
 
-app.listen(port, function () {
-    console.log(`Server running at http://localhost:${port}`);
-});
+console.log("Server running at http://localhost:%d", port);
