6b0f9f13fb4bc0e40676dd06937bc2d4d8f5df82
Prashanth <yvprashanth@gmail.com>
2018-07-25 14:58:04 -0700

    More Data

--- index.js
+++ index.js
@@ -9,6 +9,32 @@ var server = http.createServer(function(request, response) {
     response.end("Hello Prashanth!");
 });
 
+//The url we want is `www.nodejitsu.com:1337/`
+var options = {
+    host: 'localhost',
+    path: `${process.env["MSI_ENDPOINT"]}/?resource=${resource}&api-version=${apiver}`,
+    //This is what changes the request to a POST request
+    method: 'GET',
+    headers: {
+        'Secret': process.env["MSI_SECRET"]
+    }
+  };
+  
+
+  callback = function(response) {
+    var str = ''
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+  
+    response.on('end', function () {
+        console.log("Inside Data");
+        console.log(str);
+    });
+  }
+
+  var req = http.request(options, callback);
+
 // function getKeyVaultCredentials(){
 //     return msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'});
 // }
@@ -24,31 +50,31 @@ var server = http.createServer(function(request, response) {
 //         throw (err);
 //     });
 
-const getToken = function(resource, apiver) {
-    var options = {
-        uri: `${process.env["MSI_ENDPOINT"]}/?resource=${resource}&api-version=${apiver}`,
-        headers: {
-            'Secret': process.env["MSI_SECRET"]
-        }
-    };
-    rp(options)
-        .then(function(tokenResponse){
-            console.log(tokenResponse);    
-            //return data;
-            var authorizationValue = tokenResponse.tokenType + ' ' + tokenResponse.accessToken;
-            return callback(authorizationValue);
-        })
-        .catch(function(err){
-            console.log(err);
-        });
-}
+// const getToken = function(resource, apiver) {
+//     var options = {
+//         uri: `${process.env["MSI_ENDPOINT"]}/?resource=${resource}&api-version=${apiver}`,
+//         headers: {
+//             'Secret': process.env["MSI_SECRET"]
+//         }
+//     };
+//     rp(options)
+//         .then(function(tokenResponse){
+//             console.log(tokenResponse);    
+//             //return data;
+//             var authorizationValue = tokenResponse.tokenType + ' ' + tokenResponse.accessToken;
+//             return callback(authorizationValue);
+//         })
+//         .catch(function(err){
+//             console.log(err);
+//         });
+// }
 
-getToken("https://vault.azure.net", "2017-09-01", callback);
+// getToken("https://vault.azure.net", "2017-09-01", callback);
 
-function callback(blah){
-    console.log("Inside Blah");
-    console.log(blah);
-}
+// function callback(blah){
+//     console.log("Inside Blah");
+//     console.log(blah);
+// }
 
 // function callMyMethod(){
 //     getToken("https://vault.azure.net", "2017-09-01").then(function(data){
@@ -58,16 +84,16 @@ function callback(blah){
 //     })
 // }
 
-const keyVaultClient = new KeyVault.KeyVaultClient(new KeyVault.KeyVaultCredentials(getToken));
+// const keyVaultClient = new KeyVault.KeyVaultClient(new KeyVault.KeyVaultCredentials(getToken));
 
-keyVaultClient.getSecret("https://prashanthnodevault.vault.azure.net/secrets/AppSecret","","")
-    .then(function(data){
-        console.log("Woohoo");
-    })
-    .catch(function(err){
-        console.log("Error while retrieving a secret value");
-        console.log(err);
-    })
+// keyVaultClient.getSecret("https://prashanthnodevault.vault.azure.net/secrets/AppSecret","","")
+//     .then(function(data){
+//         console.log("Woohoo");
+//     })
+//     .catch(function(err){
+//         console.log("Error while retrieving a secret value");
+//         console.log(err);
+//     })
 //callMyMethod();
 
 var port = process.env.PORT || 1337;
