08fcfd8c401d6802963f3f49a227b43def5780fc
Prashanth <yvprashanth@gmail.com>
2018-07-25 20:43:06 -0700

    Set and get secret value

--- index.js
+++ index.js
@@ -14,22 +14,19 @@ msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'}).then( 
 
     var vaultUri = "https://" + "PrashanthNodeVault" + ".vault.azure.net/";
     
-    keyVaultClient.getSecret(vaultUri, "AppSecret", "").then(function(response){
-        console.log(response);    
-    })
     // We're setting the Secret value here and retrieving the secret value
-    // keyVaultClient.setSecret(vaultUri, 'my-secret', 'test-secret-value', {})
-    //     .then( (kvSecretBundle, httpReq, httpResponse) => {
-    //         console.log("Secret id: '" + kvSecretBundle.id + "'.");
-    //         return keyVaultClient.getSecret(kvSecretBundle.id, {});
-    //     })
-    //     .then( (bundle) => {
-    //         console.log("Successfully retrieved 'test-secret'");
-    //         console.log(bundle);
-    //     })
-    //     .catch( (err) => {
-    //         console.log(err);
-    //     });
+    keyVaultClient.setSecret(vaultUri, 'my-secret', 'test-secret-value', {})
+        .then( (kvSecretBundle, httpReq, httpResponse) => {
+            console.log("Secret id: '" + kvSecretBundle.id + "'.");
+            return keyVaultClient.getSecret(kvSecretBundle.id, {});
+        })
+        .then( (bundle) => {
+            console.log("Successfully retrieved 'test-secret'");
+            console.log(bundle);
+        })
+        .catch( (err) => {
+            console.log(err);
+        });
 });
 
 var port = process.env.PORT || 1337;
